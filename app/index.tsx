faffb845d29de744ecd2cda84832722ef31eb8f7
aamir <aamirkhan@engineer.com>
2024-08-17 15:27:48 +0500

    feat: add landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,5 @@
-import { Text, View, TouchableOpacity, ScrollView, StatusBar } from "react-native";
+import { Text, View, TouchableOpacity, ScrollView, Image, StatusBar } from "react-native";
 import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
-import NavBar from "@/components/navbar";
-import { SafeAreaView } from "react-native-safe-area-context"
-import FeatureTool from "@/components/featurIcon";
-import BackgroundEditTools from "@/components/backgroundEditTools";
-import ImageEnhanceTools from "@/components/imageEnhanceTools";
 import { Link } from "expo-router";
 export default function Index() {
 
@@ -12,11 +7,27 @@ export default function Index() {
         <View className="bg-background min-h-screen">
             <ScrollView className="px-[10px]">
 
+                <View className="w-full p-2 mt-4 items-center justify-center aspect-square">
+                    <Image className="w-full h-full object-contain" source={require("@/assets/images/landing-page-illustrator.png")} />
+                </View>
 
-                <Link href={"/(screens)"} replace>
-                    <Text className="text-5xl text-text">Get Started</Text>
+
+                <View className="">
+                    <Text style={{ fontFamily: "Poppins-SemiBold" }} className="text text-text text-[28px]">Transform images with</Text>
+                    <Text style={{ fontFamily: "Outfit-Bold" }} className="text-4xl text-accentBlue">MorphAI</Text>
+                </View>
+
+                <Text style={{ fontFamily: "Poppins-Regular" }} className="text-text mt-2">Unlock the power of AI to effortlessly transform your photos. Easily remove backgrounds, enhance details, upscale, and restore images with precision. With just a few taps, create stunning visuals and bring your creative vision to life.</Text>
+
+
+
+
+
+                <Link href={"/(screens)"} asChild>
+                    <TouchableOpacity activeOpacity={0.5} className="w-full bg-accentBlue mt-6 rounded-md items-center justify-center">
+                        <Text style={{ fontFamily: "Poppins-SemiBold" }} className="text-text p-2">Get Started</Text>
+                    </TouchableOpacity>
                 </Link>
-
 
 
             </ScrollView >
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import Svg, { Rect } from "react-native-svg";
 import ProIcon from './proIcon';
 const NavBar = () => {
     return (
-        <View className='flex-row px-[10px] items-center justify-between w-full'>
+        <View className='flex-row px-[10px] my-2 items-center justify-between w-full'>
             <Svg width="40" height="13" viewBox="0 0 40 13" fill="none" >
                 <Rect width="40" height="3" rx="1.5" fill="#D9D9D9" />
                 <Rect y="10" width="30" height="3" rx="1.5" fill="#D9D9D9" />
